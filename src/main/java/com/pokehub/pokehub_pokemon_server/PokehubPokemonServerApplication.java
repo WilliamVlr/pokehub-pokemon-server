47bd80ac2de31eb530b9a7479ac9d686b2847a16
William Valerie <williamvalerie2@gmail.com>
2025-10-03 22:15:24 +0700

    Spring boot initialization for usage

---
+++ src/main/java/com/pokehub/pokehub_pokemon_server/PokehubPokemonServerApplication.java
@@ -0,0 +1,13 @@
+package com.pokehub.pokehub_pokemon_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokehubPokemonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokehubPokemonServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokehub/pokehub_pokemon_server/PokehubPokemonServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pokehub.pokehub_pokemon_server;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled("Ignored until DB config ready")
+@SpringBootTest
+class PokehubPokemonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
